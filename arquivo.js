d80c426b9f2234e51731a33324dcb9b074cef2e7
Ulisses Duo <duoulisses@gmail.com>
2024-06-24 21:45:50 -0300

    layout ready. Add modal

--- arquivo.js
+++ arquivo.js
@@ -1,1 +1,43 @@
-// alert('worked')
+
+document.addEventListener('DOMContentLoaded', function() {
+    const submitform = document.getElementById('submitform');
+
+    function submitForm(event) {
+        event.preventDefault(); 
+        alert('The form has been submitted');
+        clearFields();
+    }
+
+    submitform.addEventListener('click', submitForm);
+
+    function clearFields() {
+        document.getElementById('submitform').value = '';
+        document.getElementById('parcelamento').value = '';
+        document.getElementById('dateinput').value = '';
+        document.getElementById('description').value = '';
+        document.getElementById('category').value = '';
+        document.getElementById('newExpense').value = '';
+        
+    }
+
+    // open modal
+    // close modal
+    // confirm submit
+    const myModal = document.getElementById('myModal')
+    const myInput = document.getElementById('myInput')
+    
+    myModal.addEventListener('shown.bs.modal', () => {
+      myInput.focus()
+    })
+
+
+
+    
+});
+
+
+
+
+
+
+
